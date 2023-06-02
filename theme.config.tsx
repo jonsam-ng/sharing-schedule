1183993b461044c97d3bb83072d9c5b6ead7001c
jonsam <jonsam.ng@foxmail.com>
2023-06-02 22:45:59 +0800

    init: 🎉 init repo

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
-}
+	logo: <span>Panda PO</span>,
+	project: {
+		link: "https://github.com/jonsam-ng/sharing-schedule",
+	},
+	chat: {
+		link: "https://discord.com",
+	},
+	docsRepositoryBase: "https://github.com/jonsam-ng/sharing-schedule",
+	footer: {
+		text: "Made by jonsam with ❤",
+	},
+};
 
-export default config
+export default config;
